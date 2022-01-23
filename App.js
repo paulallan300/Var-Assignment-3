01d60df62c39a267f7dd802a244f606868c26777
Paul Allan <65020951+paulallan300@users.noreply.github.com>
2022-01-23 20:25:13 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import X from "./X";
+import Y from "./Y";
+import Z from "./Z";
+
+
+export default function App() {
+  return (
+    <>
+    <X a="15" b="5"/>
+    <Y a="3" b="6" c="2"/>
+    <Z/>
+    </> 
+  );
+}
+
+
+
