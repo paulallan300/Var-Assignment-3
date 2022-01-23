0e61adf8641ca64f79bd8faf05509ec30114fb2f
Paul Allan <65020951+paulallan300@users.noreply.github.com>
2022-01-23 20:24:41 +0530

    Create Z.js

---
+++ Z.js
@@ -0,0 +1,14 @@
+export default function Z({a1,b1,c1,d1}){
+    function Sums(){
+        var a1=5;
+        var b1=8;
+        var c1=3;
+        var d1=7;
+        return(a1+b1+c1+d1);
+
+    }
+
+     return(
+         <p> Value is {a1}+{b1}+{c1}+{d1} is {Sums()}</p>
+     );
+}
